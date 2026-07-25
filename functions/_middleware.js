ed208433282e65b30ff482f88c520205750c7d41
zhiyi-zhi <455997835@qq.com>
2026-07-25 13:05:11 +0800

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-javascript
-export async function onRequest(context) {
-const response = await context.next();
-response.headers.set('Access-Control-Allow-Origin', '*');
-response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-return response;
-}
